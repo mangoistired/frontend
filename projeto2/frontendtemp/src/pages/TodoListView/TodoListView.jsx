27461aff866f6f6512c2f01e81978f64b4f137b5
Max Rodrigues <166177487+mangoistired@users.noreply.github.com>
2025-05-28 15:37:01 -0300

    versao final do front

--- projeto2/frontendtemp/src/pages/TodoListView/TodoListView.jsx
+++ projeto2/frontendtemp/src/pages/TodoListView/TodoListView.jsx
@@ -1,25 +1,42 @@
 import React from "react";
+import { useState } from "react";
 import "./TodoListView.css";
 import { useNavigate } from 'react-router-dom';
 
 export default function TodoListView() {
+  const [search, setSearch] = useState("");
+
   const tarefas = [
     { id: 1, nome: "Ler livro", data: "20/06/2024", status: "concluída" },
     { id: 2, nome: "Estudar React", data: "25/06/2024", status: "em andamento" },
     { id: 3, nome: "Fazer exercícios", data: "30/06/2024", status: "concluída" },
   ];
+
   const navigate = useNavigate();
+
+   const filtrarTarefas = tarefas.filter((tarefas) => 
+      tarefas.nome.toLowerCase().includes(search.toLowerCase()));
+
   return (
     <div className="list-container">
       <div className="list-header">
         <h2 className="list-title">
           Suas tarefas ⭐
         </h2>
+
+        <input
+          type="text"
+          placeholder="Pesquisar tarefa..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="search-bar"
+          />
+          
       </div>
 
       <div className="task-list">
 
-        {tarefas.map((item, index) => (
+        {filtrarTarefas.map((item, index) => (
           <div className="task-item" key={item.id}>
             <div className="task-info">
               <p>
